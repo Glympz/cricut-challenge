63f2957871901c0630cf08015cd0410ccdfea0bd
Jake Roberts <jake@glympz.net>
2026-04-13 15:00:36 -0600

    New unit tests Add unit tests covering the order discount bug.

--- Cricut.Orders/Cricut.Orders.Tests/Cricut.Orders.Api/Mappings/ToDomainModelMappingsTests.cs
+++ Cricut.Orders/Cricut.Orders.Tests/Cricut.Orders.Api/Mappings/ToDomainModelMappingsTests.cs
@@ -1,4 +1,4 @@
-﻿using AutoBogus;
+using AutoBogus;
 using Cricut.Orders.Api.Mappings;
 using Cricut.Orders.Api.ViewModels;
 using FluentAssertions;
@@ -17,6 +17,49 @@ namespace Cricut.Orders.Tests.Cricut.Orders.Api.Mappings
             var domainModel = viewModel.ToDomainModel();
             domainModel.Should().BeEquivalentTo(viewModel);
             domainModel.Id.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void NewOrderViewModel_ToDomain_TotalCalculatedCorrectly()
+        {
+            var viewModel = new NewOrderViewModel
+            {
+                Customer = new CustomerViewModel
+                {
+                    Id = 1,
+                    Name = "John Doe",
+                    Address = "123 Street",
+                    Email = "john@cool.example.com",
+                },
+                OrderItems = new[]
+                {
+                    new OrderItemViewModel
+                    {
+                        Product = new ProductViewModel
+                        {
+                            Id = 1,
+                            Name = "Product 1",
+                            Price = 13.50,
+                        },
+                        Quantity = 1,
+                    },
+                    new OrderItemViewModel
+                    {
+                        Product = new ProductViewModel
+                        {
+                            Id = 2,
+                            Name = "Product 2",
+                            Price = 11.50,
+                        },
+                        Quantity = 1,
+                    },
+                },
+            };
+
+            var expectedTotal = 22.50d;
+
+            var domainModel = viewModel.ToDomainModel();
+            domainModel.Total.Should().Be(expectedTotal);
         }
     }
 }
