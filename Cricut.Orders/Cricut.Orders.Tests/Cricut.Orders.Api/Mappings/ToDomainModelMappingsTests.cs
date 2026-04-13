6481889e01aab58bf46ffc76c0dbc845b29bef46
Jake Roberts <jake@glympz.net>
2026-04-13 15:08:16 -0600

    Cleanup unit tests Remove bug case unit tests. These are now covered by the new order unit tests.

--- Cricut.Orders/Cricut.Orders.Tests/Cricut.Orders.Api/Mappings/ToDomainModelMappingsTests.cs
+++ Cricut.Orders/Cricut.Orders.Tests/Cricut.Orders.Api/Mappings/ToDomainModelMappingsTests.cs
@@ -18,48 +18,5 @@ namespace Cricut.Orders.Tests.Cricut.Orders.Api.Mappings
             domainModel.Should().BeEquivalentTo(viewModel);
             domainModel.Id.Should().BeNull();
         }
-
-        [TestMethod]
-        public void NewOrderViewModel_ToDomain_TotalCalculatedCorrectly()
-        {
-            var viewModel = new NewOrderViewModel
-            {
-                Customer = new CustomerViewModel
-                {
-                    Id = 1,
-                    Name = "John Doe",
-                    Address = "123 Street",
-                    Email = "john@cool.example.com",
-                },
-                OrderItems = new[]
-                {
-                    new OrderItemViewModel
-                    {
-                        Product = new ProductViewModel
-                        {
-                            Id = 1,
-                            Name = "Product 1",
-                            Price = 13.50,
-                        },
-                        Quantity = 1,
-                    },
-                    new OrderItemViewModel
-                    {
-                        Product = new ProductViewModel
-                        {
-                            Id = 2,
-                            Name = "Product 2",
-                            Price = 11.50,
-                        },
-                        Quantity = 1,
-                    },
-                },
-            };
-
-            var expectedTotal = 22.50d;
-
-            var domainModel = viewModel.ToDomainModel();
-            domainModel.Total.Should().Be(expectedTotal);
-        }
     }
 }
